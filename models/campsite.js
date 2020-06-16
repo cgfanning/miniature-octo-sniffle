5fa7c8b7d1bf88c3d29a2d68485a4cd5c06efe44
cgfanning <cgottfanning@hotmail.com>
2020-06-16 10:53:19 -0600

    Mongoose Population - post to /campsites not working correctly

--- models/campsite.js
+++ models/campsite.js
@@ -15,9 +15,9 @@ const commentSchema = new Schema({
         type: String,
         required: true
     },
-    author: {
-        type: String,
-        required: true
+    aauthor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }
 }, {
     timestamps: true
